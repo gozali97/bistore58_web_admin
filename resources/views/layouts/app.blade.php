78afe1cdd0d163ab2228020b89f37dae34090043
Ahmad Gozali <58220137+gozali97@users.noreply.github.com>
2022-04-27 23:01:56 +0700

    kamera1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>BISTORE</title>
+    <title>BIKAMERA</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
