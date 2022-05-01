14a61160cf88836448b332ddf4cbfab9e69b1856
Ahmad Gozali <58220137+gozali97@users.noreply.github.com>
2022-05-01 12:02:33 +0700

    m3

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -39,14 +39,16 @@ class ProdukController extends Controller
     public function store(Request $request)
     {
         // dd($request->all());die();
-        $input = $request->all();
-
-        if ($request->hasFile('gambar')) {
-            $input['gambar'] = '/upload/produk/' . str_slug($input['nama_produk'], '-') . '.' . $request->gambar->getClientOriginalExtension();
-            $request->gambar->move(public_path('/upload/produk/'), $input['gambar']);
+        $fileName = '';
+        if ($request->gambar->getClientOriginalName()) {
+            $file = str_replace(' ', '', $request->gambar->getClientOriginalName());
+            $fileName = 'public/produk/' . date('mYdHs') . rand(1, 999) . '_' . $file;
+            $request->gambar->storeAs('public/produk', $fileName);
         }
 
-        Produk::create($input);
+        $user = Produk::create(array_merge($request->all(), [
+            'gambar' => $fileName
+        ]));
         return redirect('produk');
     }
 
