c199bad78300425a3f98384deccd5e82b3f209af
Ahmad Gozali <58220137+gozali97@users.noreply.github.com>
2022-05-26 00:03:57 +0700

    kategori2

--- routes/web.php
+++ routes/web.php
@@ -19,17 +19,11 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/payment', 'PymentController@index')->name('payment');
-
-Route::get('/payment/payment_submit', 'PymentController@payment_submit');
 Route::resource('/user', 'UserController');
 Route::resource('/produk', 'ProdukController');
 Route::resource('/transaksi', 'TransaksiController');
-Route::resource('/kategori', 'KategoriController');
 Route::get('/transaksi/batal/{id}', 'TransaksiController@batal')->name('transaksiBatal');
 Route::get('/transaksi/confirm/{id}', 'TransaksiController@confirm')->name('transaksiConfirm');
 Route::get('/transaksi/kirim/{id}', 'TransaksiController@kirim')->name('transaksiKirim');
 Route::get('/transaksi/selesai/{id}', 'TransaksiController@selesai')->name('transaksiSelesai');
-<<<<<<< HEAD
-=======
-// Route::resource('/kategori', 'KategoriController');
->>>>>>> eccb8a1e3a0ad6d1305d475bf3e950d24fc69faf
+Route::resource('/kategori', 'KategoriController');
