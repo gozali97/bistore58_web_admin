71626b131d97f148bd517c5de111b10d77146d8f
Ahmad Gozali <58220137+gozali97@users.noreply.github.com>
2022-05-30 22:43:30 +0700

    status

--- app/Http/Controllers/PymentController.php
+++ app/Http/Controllers/PymentController.php
@@ -16,7 +16,8 @@ class PymentController extends Controller
         $this->Transaksi = new Transaksi;
     }
 
-    public function index(Request $request){
+    public function index(Request $request)
+    {
         // Set your Merchant Server Key
         \Midtrans\Config::$serverKey = 'SB-Mid-server-gDMzeISDu3yi1nq2rJqb28c-';
         // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
@@ -25,7 +26,7 @@ class PymentController extends Controller
         \Midtrans\Config::$isSanitized = true;
         // Set 3DS transaction for credit card to true
         \Midtrans\Config::$is3ds = true;
-        
+
 
         $id_users = $request->get('id_users');
 
@@ -35,7 +36,7 @@ class PymentController extends Controller
         $user = User::where('id', $id_users)->first();
         $transaksi = Transaksi::where('id', $id_transaksi)->first();
 
-        if($user == NULL){
+        if ($user == NULL) {
             $user->name = "";
         }
 
@@ -54,12 +55,12 @@ class PymentController extends Controller
                 'phone' => $user->phone,
             ),
             'custom_expiry' => array(
-                'start_time' => date("Y-m-d H:i:s",$time),
-                'unit' => 'minute', 
+                'start_time' => date("Y-m-d H:i:s", $time),
+                'unit' => 'minute',
                 'duration'  => 1440
             ),
         );
-        
+
         $snapToken = \Midtrans\Snap::getSnapToken($params);
 
         // dd($snapToken);
@@ -70,56 +71,57 @@ class PymentController extends Controller
             'id_transaksi' => $id_transaksi,
         ];
 
-        return view('v_payment',$data);
+        return view('v_payment', $data);
     }
 
-    public function payment_submit(Request $request){
+    public function payment_submit(Request $request)
+    {
         $json = json_decode($request->get('json'));
         date_default_timezone_set('Asia/Jakarta');
         $id_transaksi = $request->id_transaksi;
-        
-        if(isset($json->va_numbers[0]->bank)){
-			$bank = $json->va_numbers[0]->bank;
-		}else{
-			$bank = "-";
-		}
 
-		if(isset($json->va_numbers[0]->va_number)){
-			$va_number = $json->va_numbers[0]->va_number;
-		}else{
-			$va_number = "-";
-		}
+        if (isset($json->va_numbers[0]->bank)) {
+            $bank = $json->va_numbers[0]->bank;
+        } else {
+            $bank = "-";
+        }
 
-		if(isset($json->bill_key)){
-			$bill_key = $json->bill_key;
-		}else{
-			$bill_key = "-";
-		}
+        if (isset($json->va_numbers[0]->va_number)) {
+            $va_number = $json->va_numbers[0]->va_number;
+        } else {
+            $va_number = "-";
+        }
 
-		if(isset($json->biller_code)){
-			$biller_code = $json->biller_code;
-		}else{
-			$biller_code = "-";
-		}
+        if (isset($json->bill_key)) {
+            $bill_key = $json->bill_key;
+        } else {
+            $bill_key = "-";
+        }
 
-		if(isset($json->permata_va_number)){
-			$permata_va_number = $json->permata_va_number;
-		}else{
-			$permata_va_number = "-";
-		}
+        if (isset($json->biller_code)) {
+            $biller_code = $json->biller_code;
+        } else {
+            $biller_code = "-";
+        }
 
-		if(isset($json->fraud_status)){
-			$fraud_status = $json->fraud_status;
-		}else{
-			$fraud_status = "-";
-		}
+        if (isset($json->permata_va_number)) {
+            $permata_va_number = $json->permata_va_number;
+        } else {
+            $permata_va_number = "-";
+        }
 
-		if(isset($json->payment_code)){
-			$payment_code = $json->payment_code;
-		}else{
-			$payment_code = "-";
-		}
-        
+        if (isset($json->fraud_status)) {
+            $fraud_status = $json->fraud_status;
+        } else {
+            $fraud_status = "-";
+        }
+
+        if (isset($json->payment_code)) {
+            $payment_code = $json->payment_code;
+        } else {
+            $payment_code = "-";
+        }
+
         $data = [
             'bank' => $bank,
             'va_number' => $va_number,
@@ -127,7 +129,7 @@ class PymentController extends Controller
             'biller_code' => $biller_code,
             'permata_va_number' => $permata_va_number,
             'tgl_payment' => date('Y-m-d H:i:s'),
-            'status' => 'Proses',
+            'status' => 'Pembayaran Dikonfirmasi',
             'order_id' => $json->order_id,
         ];
 
@@ -135,7 +137,7 @@ class PymentController extends Controller
 
         $batas_bayar = Transaksi::with(['details.produk', 'user'])->where('id', $id_transaksi)->first();
 
-        $date = new DateTime($batas_bayar->tgl_payment);   
+        $date = new DateTime($batas_bayar->tgl_payment);
         $date_plus = $date->modify("+1 days");
 
         $view = [
@@ -143,9 +145,6 @@ class PymentController extends Controller
             'batas_bayar' => $date_plus->format("Y-m-d H:i:s"),
         ];
 
-        return view('v_riwayat_payment',$view);
+        return view('v_riwayat_payment', $view);
     }
-
-    
-
 }
