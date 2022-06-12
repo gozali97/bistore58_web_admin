8b7f9557af68ef0aa728e0f57024a56450ff0462
Ahmad Gozali <58220137+gozali97@users.noreply.github.com>
2022-06-12 15:07:45 +0700

    name

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -54,7 +54,7 @@
           <img src="{{ asset('dist/img/profil.png') }}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Administrator</a>
+          <a href="#" class="d-block">{{ Auth::user()->name }}</a>
         </div>
       </div>
 
